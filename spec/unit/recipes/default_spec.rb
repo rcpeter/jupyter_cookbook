1c2e9e8a58f24accccd8667738b9daac2150bf22
rcpeter <60376893+rcpeter@users.noreply.github.com>
2020-03-30 18:46:33 +0530

    Update default_spec.rb

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,28 +1,22 @@
 require 'spec_helper'
-
 describe 'jupyter::default' do
   context 'When all attributes are default, on ##specify system platform and version' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(platform: '', version: '')
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-
     it 'sets up regular apt updates' do
       expect(chef_run).to periodic_apt_update('daily')
     end
-
     it 'installs python' do
       expect(chef_run).to install_python_runtime('3')
     end
-
     it 'installs jupyter' do
       expect(chef_run).to install_python_package('jupyter')
     end
-
     it 'includes the recipe to start a notebook server' do
       expect(chef_run).to include_recipe('jupyter::start_notebook')
     end

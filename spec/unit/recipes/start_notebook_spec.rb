f51e18d5c23e1f1be2741a824ac1e76916d2734e
rcpeter <60376893+rcpeter@users.noreply.github.com>
2020-03-30 18:47:02 +0530

    Update start_notebook_spec.rb

--- spec/unit/recipes/start_notebook_spec.rb
+++ spec/unit/recipes/start_notebook_spec.rb
@@ -1,24 +1,19 @@
 require 'spec_helper'
-
 describe 'jupyter::start_notebook' do
   context 'When all attributes are default, on ##specify platform and version' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(platform: '', version: '')
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-
     it 'creates the service' do
       expect(chef_run).to create_jupyter_notebook_service('default')
     end
-
     it 'enables the service' do
       expect(chef_run).to enable_jupyter_notebook_service('default')
     end
-
     it 'starts the service' do
       expect(chef_run).to start_jupyter_notebook_service('default')
     end
